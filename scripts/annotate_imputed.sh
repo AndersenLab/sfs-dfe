140c15dd10301faa41188bc179365846b20f153d
Daniel Cook <danielecook@gmail.com>
2017-10-08 14:01:03 -0500

    add sp34 merge

--- scripts/annotate_imputed.sh
+++ scripts/annotate_imputed.sh
@@ -18,9 +18,19 @@ tabix -p bed ${base_path}/data/operons.bed.gz
 wget ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/annotation/RNASeq_controls_FPKM/c_elegans.canonical_bioproject.current_development.RNASeq_controls_FPKM.dat -O ${base_path}/data/RNASeq_FPKM.txt
 Rscript ${base_path}/scripts/process_RNAseq_data.R
 
+# Merge in sp_34
+bcftools reheader -s <(echo 'sp34') ${base_path}/data/sp34_results/vcf/merged.vcf.gz | \
+bcftools view -O v | sed 's/\//|/g' | \
+bcftools view -O z  > ${base_path}/data/sp34_results/vcf/sp34.vcf.gz
+bcftools index ${base_path}/data/sp34_results/vcf/sp34.vcf.gz
+bcftools merge -O z \
+               -m all \
+               ${base_path}/data/WI.20170531.impute.vcf.gz \
+               ${base_path}/data/sp34_results/vcf/sp34.vcf.gz > ${base_path}/data/WI.20170531.sp34.impute.vcf.gz
+
 # Perform snpeff annotation
 cd ${base_path}/data/
-vcfanno vcfanno.toml WI.20170531.impute.vcf.gz | \
+vcfanno vcfanno.toml ${base_path}/data/WI.20170531.sp34.impute.vcf.gz | \
 bcftools filter --set-GTs . --include 'GT != "0|1"' | \
 # Used to determine location of promoter
 snpeff eff -upDownStreamLen 2000 WS256 - | \
