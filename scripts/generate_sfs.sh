7561e76cb8a6654ea09334c6c01a37df0294f748
Daniel Cook <danielecook@gmail.com>
2017-10-11 00:13:15 -0500

    additional cleanup

--- scripts/generate_sfs.sh
+++ scripts/generate_sfs.sh
@@ -5,7 +5,7 @@ base_path=`git rev-parse --show-toplevel`
 mkdir -p ${base_path}/results/sp34
 mkdir -p ${base_path}/results/QX1211
 mkdir -p ${base_path}/results/XZ1516
-mkdir -p ${base_path}/reports
+mkdir -p ${base_path}/results/reports
 
 # sp 34
 bcftools view ${base_path}/data/vcf/WI.20170531.sp34.impute.snpeff.vcf.gz | \
@@ -20,4 +20,7 @@ bcftools view ${base_path}/data/vcf/WI.20170531.impute.snpeff.vcf.gz | \
 python run_sfs.py XZ1516
 
 # Generate reports
-Rscript -e "rmarkdown::render('${base_path}/scripts/plot_sfs.Rmd')" sp34
+for outgroup in sp34; #QX1211 XZ1516;
+do
+    Rscript -e "rmarkdown::render('${base_path}/scripts/plot_sfs.Rmd', output_file = '${base_path}/results/reports/${outgroup}.html')" ${outgroup}
+done;
