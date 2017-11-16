37ff416cad7bc8861c2514e74d879ea60108ca8f
Daniel Cook <danielecook@gmail.com>
2017-11-16 14:26:24 -0600

    fix dauer

---
+++ scripts/process/dauer.R
@@ -0,0 +1,15 @@
+#!/usr/env/Rscript
+
+library(tidyverse)
+try(setwd(dirname(rstudioapi::getActiveDocumentContext()$path)))
+setwd(system("git rev-parse --show-toplevel", intern=T))
+
+gene_ids <- readr::read_csv("ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/annotation/geneIDs/c_elegans.PRJNA13758.WS256.geneIDs.txt.gz",
+                            col_names = c("n", "wb_id", "locus", 'ID', 'status'))
+
+df <- readxl::read_excel("data/gene_set/dauer_genes.xls") %>% 
+      dplyr::select(`ID` = 2) %>%
+      dplyr::mutate(ID = str_extract(ID, "[^#]+")) %>%
+      dplyr::inner_join(gene_ids) 
+      
+readr::write_tsv(df %>% dplyr::select(wb_id), 'data/gene_set/dauer_genes.txt', col_names = F)
